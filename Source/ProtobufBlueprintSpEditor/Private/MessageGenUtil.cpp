2bc374137a2bdc1605298cef72f1878ecafcb252
XTL676 <tianluoqaq@qq.com>
2024-01-18 14:37:11 +0800

    [添加] 一些路径帮助函数

--- Source/ProtobufBlueprintSpEditor/Private/MessageGenUtil.cpp
+++ Source/ProtobufBlueprintSpEditor/Private/MessageGenUtil.cpp
@@ -38,11 +38,11 @@ TMap<FString, int32> UMessageGenUtil::ReadMessageIdentifies(const FString& Ident
 }
 
 void UMessageGenUtil::WriteMessageIdentifies(const TMap<FString, int32>& MessageIdentifies,
-	const FString& IdentifiesPath)
+                                             const FString& IdentifiesPath, const bool bIsCreate)
 {
-	if (MessageIdentifies.Num() <= 0) return;
+	if (!bIsCreate && MessageIdentifies.Num() <= 0) return;
 	if (!IdentifiesPath.EndsWith("FMessageIdentifies.h")) return;
-	
+
 	FString FileContent;
 
 	// 将额外的结构内容加入字符串
@@ -64,11 +64,12 @@ void UMessageGenUtil::WriteMessageIdentifies(const TMap<FString, int32>& Message
 }
 
 void UMessageGenUtil::WriteRegisteredMessages(const TArray<FString>& MessageArray,
-	const FString& RegisteredMessagesPath, const FString& PbPrefixPath, const FString& MessageNamespace)
+                                              const FString& RegisteredMessagesPath, const FString& PbPrefixPath,
+                                              const FString& MessageNamespace, const bool bIsCreate)
 {
-	if (MessageArray.Num() <= 0) return;
+	if (!bIsCreate && MessageArray.Num() <= 0) return;
 	if (!RegisteredMessagesPath.EndsWith("FRegisteredMessages.h")) return;
-	
+
 	FString FileContent;
 
 	// 将额外的结构内容加入字符串
@@ -92,19 +93,21 @@ void UMessageGenUtil::WriteRegisteredMessages(const TArray<FString>& MessageArra
 			FileContent += FString::Printf(TEXT("#include \"%s/%s.pb.h\"\n"), *PbPrefixPath, *Message);
 		}
 	}
-	
+
 	// 生成RegisteredMessages的内容
 	FileContent += FString::Printf(TEXT("\nclass FRegisteredMessages\n"));
 	FileContent += FString::Printf(TEXT("{\n"));
 	FileContent += FString::Printf(TEXT("public:\n"));
-	FileContent += FString::Printf(TEXT("\tinline static const TMap<int32, TSharedPtr<google::protobuf::Message>> RegisteredMessages{\n"));
+	FileContent += FString::Printf(
+		TEXT("\tinline static const TMap<int32, TSharedPtr<google::protobuf::Message>> RegisteredMessages{\n"));
 
 	// 写入Map值
 	if (MessageNamespace.IsEmpty())
 	{
 		for (const FString& Message : MessageArray)
 		{
-			FileContent += FString::Printf(TEXT("\t\t{FMessageIdentifies::%s, MakeShared<%s>()},\n"), *Message, *Message);
+			FileContent += FString::Printf(
+				TEXT("\t\t{FMessageIdentifies::%s, MakeShared<%s>()},\n"), *Message, *Message);
 		}
 	}
 	else
@@ -115,26 +118,27 @@ void UMessageGenUtil::WriteRegisteredMessages(const TArray<FString>& MessageArra
 				TEXT("\t\t{FMessageIdentifies::%s, MakeShared<%s::%s>()},\n"), *Message, *MessageNamespace, *Message);
 		}
 	}
-	
+
 	FileContent += FString::Printf(TEXT("\t};\n"));
 	FileContent += FString::Printf(TEXT("};\n"));
 
 	// 清空文件内容并将字符串内容写入文件
-	FFileHelper::SaveStringToFile(FileContent, *RegisteredMessagesPath, FFileHelper::EEncodingOptions::ForceUTF8WithoutBOM);
+	FFileHelper::SaveStringToFile(FileContent, *RegisteredMessagesPath,
+	                              FFileHelper::EEncodingOptions::ForceUTF8WithoutBOM);
 }
 
 bool UMessageGenUtil::RunPyToCompileProto(const FString& PythonExecutable, const FString& PyPath,
-	const FString& ProtoDir, const FString& CppOut,
-	FString& OutStdOut, FString& OutStdErr)
+                                          const FString& ProtoDir, const FString& CppOut,
+                                          FString& OutStdOut, FString& OutStdErr)
 {
 	const FString CommandLineArgs = FString::Printf(TEXT("--proto_input=%s --cpp_out=%s"),
-		*ProtoDir, *CppOut);
+	                                                *ProtoDir, *CppOut);
 	const FString ExeCommandLine = FString::Printf(TEXT("\"%s\" \"%s\""),
-		*PythonExecutable, *PyPath);
+	                                               *PythonExecutable, *PyPath);
 
 	FString OutStdOutStr;
 	FString OutStdErrStr;
-	
+
 	int32 ReturnCode;
 	FPlatformProcess::ExecProcess(*ExeCommandLine, *CommandLineArgs, &ReturnCode, &OutStdOutStr, &OutStdErrStr);
 
@@ -164,4 +168,76 @@ TArray<FString> UMessageGenUtil::GetPbFileNames(const FString& Dir, const bool b
 	}
 
 	return Results;
+}
+
+FString UMessageGenUtil::GetPluginPath()
+{
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	const FString PluginPath = FPaths::ProjectPluginsDir().Append("ProtobufBlueprintSp");
+
+	return PlatformFile.DirectoryExists(*PluginPath) ? PluginPath : FString();
+}
+
+FString UMessageGenUtil::GetMessageConstantPath()
+{
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	const FString ConstantDir = GetPluginPath().Append("/Source/ProtobufBlueprintSp/Public/Constant");
+
+	return PlatformFile.DirectoryExists(*ConstantDir) ? ConstantDir : FString();
+}
+
+FString UMessageGenUtil::GetFMessageIdentifiesPath()
+{
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	const FString FmiPath = GetMessageConstantPath().Append("/FMessageIdentifies.h");
+
+	if (!PlatformFile.FileExists(*FmiPath))
+	{
+		WriteMessageIdentifies(TMap<FString, int32>(), FmiPath, true);
+	}
+
+	return FmiPath;
+}
+
+FString UMessageGenUtil::GetFRegisteredMessagesPath()
+{
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	const FString FrmPath = GetMessageConstantPath().Append("/FRegisteredMessages.h");
+
+	if (!PlatformFile.FileExists(*FrmPath))
+	{
+		WriteRegisteredMessages(
+			TArray<FString>(),
+			FrmPath,
+			FString(),
+			FString(),
+			true
+		);
+	}
+
+	return FrmPath;
+}
+
+FString UMessageGenUtil::GetEnginePythonPath()
+{
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	// 获取引擎根目录
+	const FString EngineDir = FPaths::EngineDir();
+
+	// 构建 Python 解释器路径
+	FString PythonExePath = FPaths::Combine(*EngineDir, TEXT("Binaries/ThirdParty/Python3/{Platform}/python.exe"));
+
+	// 替换 {Platform} 为当前平台（Win64、Win32等）
+	PythonExePath.ReplaceInline(TEXT("{Platform}"), FPlatformProcess::GetBinariesSubdirectory());
+
+	return PlatformFile.FileExists(*PythonExePath) ? PythonExePath : FString();
+}
+
+FString UMessageGenUtil::GetPyGenerateScriptPath()
+{
+	IPlatformFile& PlatformFile = FPlatformFileManager::Get().GetPlatformFile();
+	const FString ScriptPath = FPaths::Combine(GetPluginPath(),
+		TEXT("/Source/ThirdParty/libprotobuf/tools/generate_for_ue.py"));
+
+	return PlatformFile.FileExists(*ScriptPath) ? ScriptPath : FString();
 }
--- Source/ProtobufBlueprintSpEditor/Public/MessageGenUtil.h
+++ Source/ProtobufBlueprintSpEditor/Public/MessageGenUtil.h
@@ -26,9 +26,11 @@ public:
 	 * 将消息名称与消息标识符映射合集覆盖写入到FMessageIdentifies.h文件中
 	 * @param MessageIdentifies 消息名称与消息标识符映射合集
 	 * @param IdentifiesPath FMessageIdentifies.h文件路径
+	 * @param bIsCreate 是否是创建文件
 	 */
 	UFUNCTION(BlueprintCallable, Category="Editor|Protobuf|Util")
-	static void WriteMessageIdentifies(const TMap<FString, int32>& MessageIdentifies, const FString& IdentifiesPath);
+	static void WriteMessageIdentifies(const TMap<FString, int32>& MessageIdentifies,
+		const FString& IdentifiesPath, const bool bIsCreate = false);
 
 	/**
 	 * 消息名称覆盖写入到FRegisteredMessages.h文件中
@@ -36,10 +38,12 @@ public:
 	 * @param RegisteredMessagesPath FRegisteredMessages.h文件路径
 	 * @param PbPrefixPath .pb.h文件的前缀路径
 	 * @param MessageNamespace 消息命名空间全称
+	 * @param bIsCreate 是否是创建文件
 	 */
 	UFUNCTION(BlueprintCallable, Category="Editor|Protobuf|Util")
 	static void WriteRegisteredMessages(const TArray<FString>& MessageArray, const FString& RegisteredMessagesPath,
-	                                    const FString& PbPrefixPath, const FString& MessageNamespace);
+	                                    const FString& PbPrefixPath, const FString& MessageNamespace,
+	                                    const bool bIsCreate = false);
 
 	/**
 	 * 运行py脚本以编译.proto文件
@@ -64,4 +68,52 @@ public:
 	 */
 	UFUNCTION(BlueprintCallable, Category="Editor|Protobuf|Util")
 	static TArray<FString> GetPbFileNames(const FString& Dir, const bool bIsCC = false);
+
+	///------------------------- Path Helper -------------------------
+	
+	/**
+	 * 获取插件全路径
+	 * @return 获取插件全路径
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Editor|Protobuf|Util")
+	static FString GetPluginPath();
+
+	/**
+	 * 获取消息常量定义路径(插件内)
+	 * @return 消息常量定义路径(插件内)
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Editor|Protobuf|Util")
+	static FString GetMessageConstantPath();
+
+	/**
+	 * 获取FMessageIdentifies.h文件路径
+	 *
+	 * 如果不存在自动创建
+	 * @return FMessageIdentifies.h文件路径
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Editor|Protobuf|Util")
+	static FString GetFMessageIdentifiesPath();
+
+	/**
+	 * 获取FRegisteredMessages.h文件路径
+	 *
+	 * 如果不存在自动创建
+	 * @return FRegisteredMessages.h文件路径
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Editor|Protobuf|Util")
+	static FString GetFRegisteredMessagesPath();
+
+	/**
+	 * 获取引擎自带的python.exe路径
+	 * @return 引擎自带的python.exe路径
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Editor|Protobuf|Util")
+	static FString GetEnginePythonPath();
+
+	/**
+	 * 获取generate_for_ue.py路径
+	 * @return generate_for_ue.py路径
+	 */
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="Editor|Protobuf|Util")
+	static FString GetPyGenerateScriptPath();
 };
